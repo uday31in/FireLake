19f906a4b81d37e8158f62365d228487b17866ff
Uday Pandya <udpandya@microsoft.com>
2019-08-23 20:42:26 +0100

    .

--- actions/createpullrequest/entrypoint.sh
+++ actions/createpullrequest/entrypoint.sh
@@ -3,7 +3,24 @@ ls -alh
 printenv
 
 if [ -n "$(git status --porcelain)" ]; then
-  echo "there are changes";
+  
+  echo "Git Working Tree is dirty"
+  
+  echo "git branch"
+  git branch
+  
+  echo "git status"
+  git status
+  
+  echo "git add ."
+  git add .
+  
+  echo "git status"
+  git status
+  
+  echp "git commit"  
+  git commit -m "Automated PR from Build"
+  
 else
   echo "Git Working Tree Clean. Nothing to Commit";
 fi
