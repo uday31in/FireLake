ca1dffaa73ea3d6f2d2956e295b9560c0551a85c
Uday Pandya <udpandya@microsoft.com>
2019-08-23 22:11:25 +0100

    .

--- actions/createpullrequest/entrypoint.sh
+++ actions/createpullrequest/entrypoint.sh
@@ -3,29 +3,42 @@ ls -alh
 printenv
 
 if [ -n "$(git status --porcelain)" ]; then
-  
-  echo "Git Working Tree is dirty"
-  
-  branchname=$(cat /proc/sys/kernel/random/uuid)
-  
-  git config --global user.email {{GITHUB_CONTEXT.event.pusher.email }}
-  git config --global user.name {{GITHUB_CONTEXT.event.pusher.name }}
-  
-  echo "git branch"
-  git branch
-  
-  echo "git status"
-  git status
-  
-  echo "git add ."
-  git add .
-  
-  echo "git status"
-  git status
-  
-  echp "git commit"  
-  git commit -m "Automated PR from Build"
-  
+
+    echo "Git Working Tree is dirty"
+
+    #branchname=$(cat /proc/sys/kernel/random/uuid)
+
+    git config --global user.email {{GITHUB_CONTEXT.event.pusher.email }}
+    git config --global user.name {{GITHUB_CONTEXT.event.pusher.name }}
+
+    remote="uday31in-patch-1"
+    git ls-remote --exit-code --heads origin $remote
+    if [ $? -eq 0 ]; then
+        echo "branch $remote exists"
+        git checkout $remote
+    else
+        echo "branch $remote do not exists"
+        git checkout -b $remote
+    fi
+    
+    echo "git status"
+    git status
+
+    echo "git add ."
+    git add .
+
+    echo "git status"
+    git status
+
+    echo "git commit"
+    git commit -m "Automated PR from Build"
+
+    echo "git status"
+    git status
+	
+	echo "git push"
+	git push -u origin $remote
+
 else
-  echo "Git Working Tree Clean. Nothing to Commit";
+    echo "Git Working Tree Clean. Nothing to Commit"
 fi
